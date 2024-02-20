2b2726ab6554ce40e60b0330b86f851bbeef5301
Ho YingChang <yingchang.ho@gmail.com>
2024-02-20 15:09:35 +0800

    add folders

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fb_auth_provider/pages/home_page.dart';
+import 'package:fb_auth_provider/pages/signin_page.dart';
+import 'package:fb_auth_provider/pages/signup_page.dart';
+import 'package:fb_auth_provider/pages/splash_page.dart';
+import 'package:fb_auth_provider/providers/auth/auth_provider.dart';
+import 'package:fb_auth_provider/providers/profile/profile_provider.dart';
+import 'package:fb_auth_provider/providers/signin/signin_provider.dart';
+import 'package:fb_auth_provider/providers/signup/signup_provider.dart';
+import 'package:fb_auth_provider/repositories/auth_repository.dart';
+import 'package:fb_auth_provider/repositories/profile_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart' as fbAuth;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider<AuthRepository>(
+          create: (context) => AuthRepository(
+            firebaseFirestore: FirebaseFirestore.instance,
+            firebaseAuth: fbAuth.FirebaseAuth.instance,
+          ),
+        ),
+        Provider<ProfileRepository>(
+          create: (context) => ProfileRepository(
+            firebaseFirestore: FirebaseFirestore.instance,
+          ),
+        ),
+        StreamProvider<fbAuth.User?>(
+          create: (context) => context.read<AuthRepository>().user,
+          initialData: null,
+        ),
+        ChangeNotifierProxyProvider<fbAuth.User?, AuthProvider>(
+          create: (context) => AuthProvider(
+            authRepository: context.read<AuthRepository>(),
+          ),
+          update: (
+            BuildContext context,
+            fbAuth.User? userStream,
+            AuthProvider? authProvider,
+          ) =>
+              authProvider!..update(userStream),
+        ),
+        ChangeNotifierProvider<SigninProvider>(
+          create: (context) => SigninProvider(
+            authRepository: context.read<AuthRepository>(),
+          ),
+        ),
+        ChangeNotifierProvider<SignupProvider>(
+          create: (context) => SignupProvider(
+            authRepository: context.read<AuthRepository>(),
+          ),
+        ),
+        ChangeNotifierProvider<ProfileProvider>(
+          create: (context) => ProfileProvider(
+            profileRepository: context.read<ProfileRepository>(),
+          ),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Auth Provider',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: SplashPage(),
+        routes: {
+          SignupPage.routeName: (context) => SignupPage(),
+          SigninPage.routeName: (context) => SigninPage(),
+          HomePage.routeName: (context) => HomePage(),
+        },
+      ),
+    );
+  }
+}
---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,33 @@
+import 'package:fb_auth_provider/pages/home_page.dart';
+import 'package:fb_auth_provider/pages/signin_page.dart';
+import 'package:fb_auth_provider/providers/auth/auth_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/auth/auth_state.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  static const String routeName = "/";
+
+  @override
+  Widget build(BuildContext context) {
+    final authState = context.watch<AuthProvider>().state;
+
+    if (authState.authStatus == AuthStatus.authenticated) {
+      WidgetsBinding.instance!.addPostFrameCallback((_) {
+        Navigator.pushNamed(context, HomePage.routeName);
+      });
+    } else if (authState.authStatus == AuthStatus.unauthenticated) {
+      WidgetsBinding.instance!.addPostFrameCallback((_) {
+        Navigator.pushNamed(context, SigninPage.routeName);
+      });
+    }
+    return Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
